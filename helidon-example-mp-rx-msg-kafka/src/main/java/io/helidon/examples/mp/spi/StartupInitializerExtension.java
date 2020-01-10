101483d82fe16ac777b5da4b173e1e8bbd22a641
Jobinesh Purushothaman <jobinesh.purushothaman@oracle.com>
2020-01-09 17:19:11 -0800

    changed proj name

---
+++ helidon-example-mp-rx-msg-kafka/src/main/java/io/helidon/examples/mp/spi/StartupInitializerExtension.java
@@ -0,0 +1,23 @@
+package io.helidon.examples.mp.spi;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.enterprise.inject.spi.*;
+import java.util.ArrayList;
+import java.util.List;
+public class StartupInitializerExtension implements Extension {
+    private List<Bean<?>> eagerBeansList = new ArrayList<Bean<?>>();
+
+    public <T> void collect(@Observes ProcessBean<T> event) {
+        if (event.getAnnotated().isAnnotationPresent(Startup.class)
+                && event.getAnnotated().isAnnotationPresent(ApplicationScoped.class)) {
+            eagerBeansList.add(event.getBean());
+        }
+    }
+
+    public void load(@Observes AfterDeploymentValidation event, BeanManager beanManager) {
+        for (Bean<?> bean : eagerBeansList) {
+            // note: toString() is important to instantiate the bean
+            beanManager.getReference(bean, bean.getBeanClass(), beanManager.createCreationalContext(bean)).toString();
+        }
+    }
+}
